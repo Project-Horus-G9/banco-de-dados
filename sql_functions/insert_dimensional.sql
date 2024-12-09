b0460b8a5d5e643cc9780d7bd9d8b5558d876a34
shift39 <marco.magalhaes@sptech.school>
2024-12-08 23:59:20 -0300

    aprimoramentos do db

--- sql_functions/insert_dimensional.sql
+++ sql_functions/insert_dimensional.sql
@@ -1,120 +1,56 @@
--- Inserção de dados na tabela dim_estado
-INSERT INTO dim_estado (estado)
-SELECT DISTINCT estado
+INSERT INTO horus_dimensional.dim_estado (estado)
+SELECT DISTINCT e.estado
+FROM horus_trusted.endereco e;
+
+INSERT INTO horus_dimensional.dim_cidade (cidade, fk_estado)
+SELECT DISTINCT e.cidade, es.id_estado
 FROM horus_trusted.endereco e
-WHERE estado IS NOT NULL
-  AND NOT EXISTS (
-      SELECT 1
-      FROM dim_estado ds
-      WHERE ds.estado = e.estado
-  );
+JOIN horus_dimensional.dim_estado es ON es.estado = e.estado;
 
--- Inserção de dados na tabela dim_cidade
-INSERT INTO dim_cidade (cidade, fk_estado)
-SELECT DISTINCT e.cidade, ds.id_estado
-FROM horus_trusted.endereco e
-JOIN dim_estado ds ON ds.estado = e.estado
-WHERE cidade IS NOT NULL
-  AND NOT EXISTS (
-      SELECT 1
-      FROM dim_cidade dc
-      WHERE dc.cidade = e.cidade
-        AND dc.fk_estado = ds.id_estado
-  );
+INSERT INTO horus_dimensional.dim_empresa (nome, fk_cidade)
+SELECT e.nome, ec.id_cidade
+FROM horus_trusted.empresa e
+JOIN horus_dimensional.dim_cidade ec ON ec.cidade = (SELECT cidade FROM horus_trusted.endereco WHERE fk_empresa = e.id_empresa LIMIT 1);
 
--- Inserção de dados na tabela dim_empresa
-INSERT INTO dim_empresa (nome, fk_cidade)
-SELECT DISTINCT nome, 
-                (SELECT id_cidade FROM dim_cidade WHERE cidade = e.cidade AND fk_estado = es.id_estado LIMIT 1)
-FROM horus_trusted.empresa emp
-JOIN horus_trusted.endereco e ON e.id_endereco = emp.fk_endereco
-JOIN dim_estado es ON es.estado = e.estado
-WHERE nome IS NOT NULL
-  AND NOT EXISTS (
-      SELECT 1
-      FROM dim_empresa de
-      WHERE de.nome = emp.nome
-  );
-
--- Inserção de dados na tabela dim_setor
-INSERT INTO dim_setor (nome, fk_empresa)
-SELECT DISTINCT s.nome, de.id_empresa
+INSERT INTO horus_dimensional.dim_setor (nome, fk_empresa)
+SELECT s.nome, de.id_empresa
 FROM horus_trusted.setor s
-JOIN horus_trusted.empresa e ON s.fk_empresa = e.id_empresa
-JOIN dim_empresa de ON de.nome = e.nome
-WHERE s.nome IS NOT NULL
-  AND NOT EXISTS (
-      SELECT 1
-      FROM dim_setor ds
-      WHERE ds.nome = s.nome
-        AND ds.fk_empresa = de.id_empresa
-  );
+JOIN horus_dimensional.dim_empresa de ON de.id_empresa = s.fk_empresa;
 
--- Inserção de dados na tabela dim_painel
-INSERT INTO dim_painel (nome, fk_setor)
-SELECT DISTINCT p.nome, ds.id_setor
+INSERT INTO horus_dimensional.dim_painel (nome, fk_setor)
+SELECT p.nome, ds.id_setor
 FROM horus_trusted.painel p
-JOIN horus_trusted.setor s ON p.fk_setor = s.id_setor
-JOIN dim_setor ds ON ds.nome = s.nome
-WHERE p.nome IS NOT NULL
-  AND NOT EXISTS (
-      SELECT 1
-      FROM dim_painel dp
-      WHERE dp.nome = p.nome
-        AND dp.fk_setor = ds.id_setor
-  );
+JOIN horus_dimensional.dim_setor ds ON ds.id_setor = p.fk_setor;
 
--- Inserção de dados na tabela dim_tempo
-INSERT INTO dim_tempo (horario, dia, mes, ano)
-SELECT DISTINCT TIME(dl.data), DAY(dl.data), MONTH(dl.data), YEAR(dl.data)
-FROM horus_trusted.dados_leitura dl
-WHERE dl.data IS NOT NULL
-  AND NOT EXISTS (
-      SELECT 1
-      FROM dim_tempo dt
-      WHERE dt.horario = TIME(dl.data)
-        AND dt.dia = DAY(dl.data)
-        AND dt.mes = MONTH(dl.data)
-        AND dt.ano = YEAR(dl.data)
-  );
+INSERT INTO horus_dimensional.dim_tempo (horario, dia, mes, ano)
+SELECT DISTINCT
+    DATE_FORMAT(dl.data, '%H:%i:%s') AS horario,
+    DAY(dl.data) AS dia,
+    MONTH(dl.data) AS mes,
+    YEAR(dl.data) AS ano
+FROM horus_trusted.dados_leitura dl;
 
--- Inserção dos dados na tabela fato_dados
-INSERT INTO fato_dados (
-    id_tempo,
-    id_painel,
-    id_orientacao,
-    id_ceu,
-    m_temp_interna,
-    m_temp_externa,
-    m_energia_esperada,
-    m_energia_gerada,
-    m_luminosidade,
-    m_eficiencia,
-    m_umidade,
-    m_tensao,
-    m_obstrucao
+INSERT INTO horus_dimensional.dim_orientacao (direcionamento, inclinacao)
+SELECT DISTINCT dl.direcionamento, dl.inclinacao
+FROM horus_trusted.dados_leitura dl;
+
+INSERT INTO horus_dimensional.dim_ceu (ceu)
+SELECT DISTINCT dl.ceu
+FROM horus_trusted.dados_leitura dl;
+
+INSERT INTO horus_dimensional.fato_dados (
+    id_tempo, id_painel, id_orientacao, id_ceu,
+    m_temp_interna, m_temp_externa, m_energia_esperada,
+    m_energia_gerada, m_luminosidade, m_eficiencia,
+    m_umidade, m_tensao, m_obstrucao
 )
 SELECT
-    dt.id_tempo,
-    dp.id_painel,
-    dor.id_orientacao,
-    du.id_ceu,
-    dl.temperatura_interna,
-    dl.temperatura_externa,
-    dl.energia_esperada,
-    dl.energia_gerada,
-    dl.luminosidade,
-    dl.eficiencia,
-    dl.umidade,
-    dl.tensao,
-    dl.obstrucao
+    dt.id_tempo, dp.id_painel, do.id_orientacao, dc.id_ceu,
+    dl.temperatura_interna, dl.temperatura_externa, dl.energia_esperada,
+    dl.energia_gerada, dl.luminosidade, dl.eficiencia,
+    dl.umidade, dl.tensao, dl.obstrucao
 FROM horus_trusted.dados_leitura dl
-JOIN dim_tempo dt ON dt.horario = TIME(dl.data)
-                 AND dt.dia = DAY(dl.data)
-                 AND dt.mes = MONTH(dl.data)
-                 AND dt.ano = YEAR(dl.data)
-JOIN dim_painel dp ON dp.nome = (SELECT nome FROM horus_trusted.painel WHERE id_painel = dl.fk_painel)
-JOIN dim_orientacao dor ON dor.direcionamento = dl.direcionamento
-                        AND dor.inclinacao = dl.inclinacao
-JOIN dim_ceu du ON du.ceu = dl.ceu
-WHERE dl.temperatura_interna IS NOT NULL;
+JOIN horus_dimensional.dim_tempo dt ON dt.dia = DAY(dl.data) AND dt.mes = MONTH(dl.data) AND dt.ano = YEAR(dl.data)
+JOIN horus_dimensional.dim_painel dp ON dp.id_painel = dl.fk_painel
+JOIN horus_dimensional.dim_orientacao do ON do.direcionamento = dl.direcionamento AND do.inclinacao = dl.inclinacao
+JOIN horus_dimensional.dim_ceu dc ON dc.ceu = dl.ceu;
